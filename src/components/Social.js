23d6a472659ce83896077d3757fcc7495ef0e212
bernardoclaure <bernardoclaure13@gmail.com>
2022-04-05 09:56:29 -0400

    deleted commented lines, and rar file

--- src/components/Social.js
+++ src/components/Social.js
@@ -4,8 +4,7 @@ import YouTubeIcon from '@mui/icons-material/YouTube';
 import FacebookRoundedIcon from '@mui/icons-material/FacebookRounded';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
-// if you wanto to add twitter
-// import TwitterIcon from '@material-ui/icons/Twitter';
+
 
 import { socialMedia } from "../data/socialMedia";
 
@@ -23,24 +22,9 @@ const useStyles = makeStyles((theme) => ({
 
 const Social = ({ color }) => {
   const classes = useStyles();
-  // if you want to add more social medias, add it to here and /data/socialMedia.js.
-  // and import the Material Icon, then add the code.
+ 
   const { instagram, facebook, github, homepage } = socialMedia;
 
-  // if you add twitter , it will be
-  // const { instagram, facebook, github, homepage, twitter } = socialMedia;
-  {
-    //  and add this code to line 98,
-    /* <Grid
-        item
-        component={"a"}
-        target="_blank"
-        rel="noreferrer noopener"
-        href={twitter}
-      >
-       <TwitterIcon className={classes.snsIcon} />
-    </Grid> */
-  }
   return (
     <Grid item container spacing={2} justify="center">
       <Grid
@@ -91,7 +75,6 @@ const Social = ({ color }) => {
           color={color ? "primary" : "secondary"}
         />
       </Grid>
-      {/* add social media*/}
     </Grid>
   );
 };
